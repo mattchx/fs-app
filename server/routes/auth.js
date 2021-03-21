f8255336beaaaa6ae079b36f930a9f07893459bb
Matt <mattcharlesh@gmail.com>
2021-03-21 14:33:45 -0400

    validation basic setup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,28 @@ const express = require('express')
 const router = express.Router()
 const User = require('../model/User')
 
+// VALIDATION
+const Joi = require('joi')
+const { valid } = require('joi')
+
+const schema = Joi.object({
+    name: Joi.string()
+    .min(6)
+    .required(),
+    email: Joi.string()
+    .min(6)
+    .required()
+    .email(),
+    password: Joi.string()
+    .min(6)
+    .required()
+})
+
 router.post('/signup', async (req, res) => {
+
+    // LETS VALIDATE DATA BEFORE MAKING USER
+    const {error} = schema.validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
     const user = new User({
         name: req.body.name,
         email: req.body.email,
