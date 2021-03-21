73120d3403a2993ab9ae77ccf74b90629b3dc522
Matt <mattcharlesh@gmail.com>
2021-03-20 20:37:39 -0400

    connect to DB and add User schema

--- server/app.js
+++ server/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('Connected to DB')
+);
 // Import Routes
- const authRoute = require('./routes/auth');
+const authRoute = require('./routes/auth');
 
 // Route Middlewares
- app.use('/api/user', authRoute);
- 
+app.use('/api/user', authRoute);
+
 // app.get('/', (req, res) => {
 //     res.send('hello world')
 // })
