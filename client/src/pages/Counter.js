5d2f15fc62d9f30075b3ce2e1aa81bcfcba4faba
Matt <mattcharlesh@gmail.com>
2021-03-22 11:05:22 -0400

    set up react router with basic nav

---
+++ client/src/pages/Counter.js
@@ -0,0 +1,24 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../actions';
+import Header from '../components/Header'
+function Counter() {
+  const counter = useSelector((state) => state.counter);
+  const loggedIn = useSelector((state) => state.loggedIn);
+  const dispatch = useDispatch();
+  return (
+    <div>
+        <Header/>
+      <div>
+        <div>Counter {counter}</div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+      {loggedIn && <div>Valuable Information</div>}
+      <button onClick={() => dispatch({ type: 'LOG_IN' })}>
+        Log me {loggedIn ? 'out' : 'in'}{' '}
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
