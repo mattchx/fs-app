17a6424e060af382165022bfc6be64f7655c5fb5
Matt <mattcharlesh@gmail.com>
2021-03-26 16:17:43 -0400

    added getPosts action creator

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,21 +1,25 @@
+import axios from 'axios'
 import { DELETE_POST, ADD_POST, GET_POSTS } from './types';
 
 export const deletePost = (id) => {
   return {
     type: DELETE_POST,
-    payload: id,
+    payload: {id},
   };
 };
 
 export const addPost = (text) => {
   return {
     type: ADD_POST,
-    payload: text,
+    payload: {text},
   };
 };
 
-export const getPosts = () => {
+export const  getPosts = async () => {
+    const response = await axios.get('/user/posts')
+    const posts = await response.json()
   return {
     type: GET_POSTS,
+    payload: {posts}
   };
 };
