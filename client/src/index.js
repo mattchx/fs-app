37d66347a052fccd8eec2c32dce75f9c5acef4c4
Matt <mattcharlesh@gmail.com>
2021-03-26 15:07:06 -0400

    added redux-thunk for async requests

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
 import rootReducer from './reducers';
 
 const store = createStore(
-  rootReducer,
+  rootReducer, applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
